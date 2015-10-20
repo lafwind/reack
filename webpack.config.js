eab7468a7fc845494752a2a963510dd6bc4a87fd
Lafwind <lafwind@gmail.com>
2015-10-20 23:23:44 +0800

    修改目录结构, 加入 babel-loader, css-loader, style-loader, sass-loader 并修改webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,39 @@
 var path = require('path');
 
 var config = {
-    entry: path.resolve(__dirname, 'app/main.js'),
+    entry: path.resolve(__dirname, './app/main.js'),
 
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js'
+    },
+
+    module: {
+        loaders: [
+            { test: /\.js$/, loader: 'babel', exclude: /node_modules/ },
+            { test: /\.css$/, loader: 'style!css' },
+            { test: /\.scss$/, loader: 'style!css!sass' }
+        ]
     }
 };
 
-module.exports =config;
+module.exports = config;
+
+// module.exports = {
+//     entry: ['webpack/hot/dev-server', './main.js'],
+//     // './app/main.js',
+
+
+//     output: {
+//         path: __dirname,
+//         filename: 'bundle.js'
+//     },
+
+//     module: {
+//         loaders: [
+//             { test: /\.js$/, loader: 'babel', exclude: /node_modules/ },
+//             { test: /\.css$/, loader: 'style!css' },
+//             { test: /\.scss$/, loader: 'style!css!sass' }
+//         ]
+//     }
+// };
